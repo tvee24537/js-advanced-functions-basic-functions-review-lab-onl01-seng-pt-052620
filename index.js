0282969b1661879244a6622b30a7b6819864a0e5
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-12-17 22:44:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function saturdayFun(arg="roller-skate") {
     return "This Saturday, I want to "+arg+"!"
 };
+
+let mondayWork = function(arg="go to the office"){
+    return "This Monday, I will "+arg+"."
+}
