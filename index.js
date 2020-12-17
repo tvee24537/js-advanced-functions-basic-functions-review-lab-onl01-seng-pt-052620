4ba128a58b9ded4931d9451afa42ee2b8a4700f1
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-12-17 23:04:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ function wrapAdjective(flair='*') {
             return `You are ${flair}${param}${flair}!`;
         } 
     }
+    
+const Calculator = {
+    add: function(a,b){return a + b},
+    subtract: function(a,b){return a - b}, 
+    multiply: function(a,b){return a * b},
+    divide: function(a,b){return a / b}
+}
+
+function actionApplyer(startint, funcarray) {
+    let y = startint;
+    for (let x of funcarray){
+        y = x(y)
+    }
+    return y;
+}; 
