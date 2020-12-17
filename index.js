151b0f9af8c5f91afd6cff0060527c170a435aca
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-12-17 22:54:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function saturdayFun(arg="roller-skate") {
 let mondayWork = function(arg="go to the office"){
     return "This Monday, I will "+arg+"."
 }
+
+function wrapAdjective(flair='*') {
+        return function(param="special") {
+            return `You are ${flair}${param}${flair}!`;
+        } 
+    }
